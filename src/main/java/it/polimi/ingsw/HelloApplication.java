329274098407f5a69502d682094d8afb8dec89b1
CarloArnone <carlo.arnone71@gmail.com>
2023-03-14 09:57:57 +0100

    First commit of communication development branch

--- src/main/java/it/polimi/ingsw/HelloApplication.java
+++ src/main/java/it/polimi/ingsw/HelloApplication.java
@@ -1,11 +1,11 @@
 package it.polimi.ingsw;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+        import javafx.application.Application;
+        import javafx.fxml.FXMLLoader;
+        import javafx.scene.Scene;
+        import javafx.stage.Stage;
 
-import java.io.IOException;
+        import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
