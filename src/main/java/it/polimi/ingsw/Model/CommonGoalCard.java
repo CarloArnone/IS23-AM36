61530584b303c6d63c82d7a9e1748a1e5aaa79e8
CarloArnone <carlo.arnone71@gmail.com>
2023-03-15 19:14:20 +0100

    Update CommonGoalCard

--- src/main/java/it/polimi/ingsw/Model/CommonGoalCard.java
+++ src/main/java/it/polimi/ingsw/Model/CommonGoalCard.java
@@ -5,9 +5,12 @@ import java.util.List;
 
 public abstract class CommonGoalCard implements Goal {
 
-    private List<Integer> points = new ArrayList<Integer>();
-
-    public abstract int getPoints(Player p, List<Integer> points);
+    private List<Integer>  points = new ArrayList<>();
+    public int getPoints(){
+        int temp = this.points.get(0);
+        this.points.remove(0);
+        return temp;
+    }
 
 
 }
