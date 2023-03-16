16a6854dd5d991cefbbc37484bc84adacb7f69c5
Doc <vincenzo.donatelli@mail.polimi.it>
2023-03-16 22:11:24 +0100

    CommonGoals updates.

--- src/main/java/it/polimi/ingsw/Model/GoalCards/TwoColDiffTypes.java
+++ src/main/java/it/polimi/ingsw/Model/GoalCards/TwoColDiffTypes.java
@@ -4,8 +4,9 @@ import it.polimi.ingsw.Model.CommonGoalCard;
 import it.polimi.ingsw.Model.ItemCard;
 import it.polimi.ingsw.Model.Player;
 
-import java.util.List;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 public class TwoColDiffTypes extends CommonGoalCard {
 
@@ -16,27 +17,55 @@ public class TwoColDiffTypes extends CommonGoalCard {
         boolean firstMatchFound = false;
         Optional<ItemCard>[][] shelfCopy = p.getMyShelf().getShelf();
 
-        for(int k = 0; k < shelfCopy.length; k++) {
-            for (int j = 0; j < shelfCopy[0].length - 1; j++) {
-                if(k == shelfCopy[0].length) return false;
-                for(int i = j+1; i < shelfCopy[0].length; i++) {
-                    if(shelfCopy[k][j].isEmpty() || shelfCopy[k][i].isEmpty()){
-                        k++;
-                        j = 0;
-                        break;
-                    }
-                    if(shelfCopy[k][j].get().getColor() == shelfCopy[k][i].get().getColor()){
-                        k++;
-                        j = 0;
-                        break;
-                    }
-                    if(i == shelfCopy[0].length - 1 && j == shelfCopy[0].length - 2){
-                        if(firstMatchFound) return true;
-                        else firstMatchFound = true;
-                    }
-                }
+        for (Optional<ItemCard>[] optionals : shelfCopy) {
+            if (checkColumn(optionals)) {
+                if (firstMatchFound) return true;
+                firstMatchFound = true;
             }
         }
         return false;
     }
+
+    private boolean checkColumn(Optional<ItemCard>[] columnCopy){
+
+        Set<Character> foundColors = new HashSet<>();
+
+        for (Optional<ItemCard> itemCard : columnCopy) {
+            if (itemCard.isEmpty()) return false;
+            foundColors.add(itemCard.get().getColor());
+        }
+        return foundColors.size() == 6;
+    }
 }
+
+
+/**
+ * public boolean checkGoal(Player p) {
+ *
+ *         boolean firstMatchFound = false;
+ *         Optional<ItemCard>[][] shelfCopy = p.getMyShelf().getShelf();
+ *
+ *         for(int k = 0; k < shelfCopy.length; k++) {
+ *             for (int j = 0; j < shelfCopy[0].length - 1; j++) {
+ *                 if(k == shelfCopy[0].length) return false;
+ *                 for(int i = j+1; i < shelfCopy[0].length; i++) {
+ *                     if(shelfCopy[k][j].isEmpty() || shelfCopy[k][i].isEmpty()){
+ *                         k++;
+ *                         j = 0;
+ *                         break;
+ *                     }
+ *                     if(shelfCopy[k][j].get().getColor() == shelfCopy[k][i].get().getColor()){
+ *                         k++;
+ *                         j = 0;
+ *                         break;
+ *                     }
+ *                     if(i == shelfCopy[0].length - 1 && j == shelfCopy[0].length - 2){
+ *                         if(firstMatchFound) return true;
+ *                         else firstMatchFound = true;
+ *                     }
+ *                 }
+ *             }
+ *         }
+ *         return false;
+ *     }
+ */
--- src/main/java/it/polimi/ingsw/Model/GoalCards/eightEquals.java
+++ src/main/java/it/polimi/ingsw/Model/GoalCards/eightEquals.java
@@ -4,10 +4,7 @@ import it.polimi.ingsw.Model.CommonGoalCard;
 import it.polimi.ingsw.Model.ItemCard;
 import it.polimi.ingsw.Model.Player;
 
-import java.util.List;
 import java.util.Optional;
-
-import static java.util.Optional.empty;
 
 public class eightEquals extends CommonGoalCard {
 
@@ -18,12 +15,12 @@ public class eightEquals extends CommonGoalCard {
         int[] matchCounter = {0, 0, 0, 0, 0, 0};
         Optional<ItemCard>[][] shelfCopy = p.getMyShelf().getShelf();
 
-        for (int i = 0; i < shelfCopy.length; i++) {
+        for (Optional<ItemCard>[] optionals : shelfCopy) {
             for (int j = 0; j < shelfCopy[0].length; j++) {
 
-                if(shelfCopy[i][j].isEmpty()) continue;
+                if (optionals[j].isEmpty()) continue;
 
-                switch(shelfCopy[i][j].get().getColor()){
+                switch (optionals[j].get().getColor()) {
                     case 'b':
                         matchCounter[0]++;
                     case 'c':
@@ -37,55 +34,55 @@ public class eightEquals extends CommonGoalCard {
                     case 'y':
                         matchCounter[5]++;
                     default:
-                        System.out.println("Error: color found = " + shelfCopy[i][j].get().getColor());
+                        System.out.println("Error: color found = " + optionals[j].get().getColor());
                 }
             }
         }
 
-        for(int i = 0; i < matchCounter.length; i++){
-            if(matchCounter[i] == 8)return true;
+        for (int j : matchCounter) {
+            if (j == 8) return true;
         }
 
         return false;
     }
-
-
-    /*@Override
-    public boolean checkGoal(Player p) {
-
-        int matchCounter = 0;
-        Optional<ItemCard>[][] shelfCopy = p.getMyShelf().getShelf();
-
-        for (int i = 0; i < shelfCopy.length; i++) {
-            for (int j = 0; j < shelfCopy[0].length; j++) {
-
-                if (shelfCopy[i][j].isEmpty()) continue;
-
-                for (int k = 0; k < shelfCopy.length; k++) {
-                    for (int t = 0; t < shelfCopy[0].length; t++) {
-
-                        if (shelfCopy[k][t].isEmpty()) continue;
-
-                        if (shelfCopy[i][j].get().getColor() == shelfCopy[k][t].get().getColor()) {
-                            matchCounter++;
-                            if (matchCounter == 7) return true;
-                        }
-                    }
-                }
-
-                for (int k = 0; k < shelfCopy.length; k++) {
-                    for (int t = 0; t < shelfCopy[0].length; t++) {
-
-                        if (shelfCopy[k][t].isEmpty()) continue;
-
-                        if (shelfCopy[i][j].get().getColor() == shelfCopy[k][t].get().getColor()) {
-                            shelfCopy[k][t] = empty();
-                        }
-                    }
-                }
-
-            }
-        }
-        return false;
-    }*/
 }
+
+/**
+ * public boolean checkGoal(Player p) {
+ *
+ *         int matchCounter = 0;
+ *         Optional<ItemCard>[][] shelfCopy = p.getMyShelf().getShelf();
+ *
+ *         for (int i = 0; i < shelfCopy.length; i++) {
+ *             for (int j = 0; j < shelfCopy[0].length; j++) {
+ *
+ *                 if (shelfCopy[i][j].isEmpty()) continue;
+ *
+ *                 for (int k = 0; k < shelfCopy.length; k++) {
+ *                     for (int t = 0; t < shelfCopy[0].length; t++) {
+ *
+ *                         if (shelfCopy[k][t].isEmpty()) continue;
+ *
+ *                         if (shelfCopy[i][j].get().getColor() == shelfCopy[k][t].get().getColor()) {
+ *                             matchCounter++;
+ *                             if (matchCounter == 7) return true;
+ *                         }
+ *                     }
+ *                 }
+ *
+ *                 for (int k = 0; k < shelfCopy.length; k++) {
+ *                     for (int t = 0; t < shelfCopy[0].length; t++) {
+ *
+ *                         if (shelfCopy[k][t].isEmpty()) continue;
+ *
+ *                         if (shelfCopy[i][j].get().getColor() == shelfCopy[k][t].get().getColor()) {
+ *                             shelfCopy[k][t] = empty();
+ *                         }
+ *                     }
+ *                 }
+ *
+ *             }
+ *         }
+ *         return false;
+ *     }
+ */
