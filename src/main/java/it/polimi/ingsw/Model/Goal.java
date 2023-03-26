9e7c955a4416de395b6c6f1b9d1a4ea077dd404f
CarloArnone <carlo.arnone@mail.polimi.it>
2023-03-26 20:15:43 +0100

    classes Tuning

--- src/main/java/it/polimi/ingsw/Model/Goal.java
+++ src/main/java/it/polimi/ingsw/Model/Goal.java
@@ -5,15 +5,12 @@ public abstract class Goal {
     int obtainedPoints = 0;
     String name;
     abstract boolean checkGoal(Player p);
-
     public int getObtainedPoints() {
         return obtainedPoints;
     }
-
     public void setObtainedPoints(int obtainedPoints) {
         this.obtainedPoints = obtainedPoints;
     }
-
     public String getName() {
         return name;
     }
--- src/main/java/it/polimi/ingsw/Model/ItemCard.java
+++ src/main/java/it/polimi/ingsw/Model/ItemCard.java
@@ -3,7 +3,7 @@ package it.polimi.ingsw.Model;
 public class ItemCard {
 
     private Character color;
-    private String image; //TODO: work in progress PD.
+    private String image;
 
     public ItemCard(char color, String image) {
         this.color = color;
@@ -17,15 +17,12 @@ public class ItemCard {
     public Character getColor() {
         return color;
     }
-
     public String getImage() {
         return image;
     }
-
     public void setImage(String image) {
         this.image = image;
     }
-
     public void setColor(Character color) {
         this.color = color;
     }
