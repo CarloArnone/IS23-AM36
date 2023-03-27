fa5df5a7252de24a86118cc1a18a05ac0f051f8b
CarloArnone <carlo.arnone@mail.polimi.it>
2023-03-27 08:27:50 +0100

    pseudo CLI Implementation for Testing

--- src/main/java/it/polimi/ingsw/Client/CLI/CLI_Launch.java
+++ src/main/java/it/polimi/ingsw/Client/CLI/CLI_Launch.java
@@ -2,7 +2,7 @@ package it.polimi.ingsw.Client.CLI;
 
 public class CLI_Launch {
     public static void main(String[] args) {
-        CLI view = new CLI(2);
+        CLI view = new CLI(0);
         view.start();
     }
 }
